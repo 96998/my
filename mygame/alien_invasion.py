a4653ab1587af6c20b303fef3175b1e476dab6e6
Alexander Jiajiason <my@example.com>
2022-04-05 11:10:04 +0800

    限制子弹数量

--- mygame/alien_invasion.py
+++ mygame/alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 
 def run_game():
@@ -13,14 +14,23 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
     ship = Ship(ai_settings, screen)
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
     # 设置背景色
     bg_color = (230, 230, 230)
     # 开始游戏主循环
     while True:
         # 监视键盘和鼠标事件
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        # bullets.update()
+        # 删除已消失的子弹
+        for bullet in bullets.copy():
+            bullet.update()
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+        print(len(bullets))
+        gf.update_screen(ai_settings, screen, ship, bullets)
         # for event in pygame.event.get():
         #     if event.type == pygame.QUIT:
         #         sys.exit()
--- mygame/game_functions.py
+++ mygame/game_functions.py
@@ -1,8 +1,9 @@
 import sys
 import pygame
+from bullet import Bullet
 
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """响应按键"""
     # 这里用elif设置,则右键的优先级比左键高
     if event.key == pygame.K_RIGHT:
@@ -11,6 +12,13 @@ def check_keydown_events(event, ship):
     elif event.key == pygame.K_LEFT:
         # 向左移动飞船
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        if len(bullets) < ai_settings.bullets_allowed:
+            new_bullet = Bullet(ai_settings, screen, ship)
+            bullets.add(new_bullet)
+
+
+# 创建
 
 
 def check_keyup_events(event, ship):
@@ -21,21 +29,23 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     """响应按键和鼠标事件"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """更新屏幕上的图像,并切换到新屏幕"""
     # 每次循环时都重绘屏幕
     screen.fill(ai_settings.bg_color)
+    for bullet in bullets.sprites():
+        bullet.draw()
     ship.blitme()
     # 让最近绘制的屏幕可见
     pygame.display.flip()
