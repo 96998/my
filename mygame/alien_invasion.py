fa2ef64fa36cd4bfc375e68d22e4508ea1c5da26
Alexander Jiajiason <my@example.com>
2022-04-05 08:30:53 +0800

    设置飞船移动速度

--- mygame/alien_invasion.py
+++ mygame/alien_invasion.py
@@ -12,7 +12,7 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
     # 设置背景色
     bg_color = (230, 230, 230)
     # 开始游戏主循环
